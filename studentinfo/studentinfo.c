5c2beef753058454ff44618cb2521111aba23d31
mbloncourt <mbloncourt1315@floridapoly.edu>
2025-01-16 13:40:00 +0000

    yeah!

---
+++ studentinfo/studentinfo.c
@@ -0,0 +1,13 @@
+/*
+The program prints the student's info in the specific format:
+*/
+#include <stdio.h>
+int main() {
+int courseID = 2271;
+int studentID = 21315;
+printf("Name: Maison Bloncourt\n");
+printf("Course ID: %d\n", courseID);
+printf("Student ID: %d\n", studentID);
+printf("Student ID - Course ID is: %d\n", studentID - courseID);
+return 0;
+}
